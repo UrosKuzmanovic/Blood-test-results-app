97119ae0ddd026ff205c9ed7a645e2aa13c4826f
UrosKuzmanovic <ukuzmanovic1@gmail.com>
2020-12-01 15:50:39 +0100

    Ionic Selectable and searching, displaying data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,29 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { Data } from '@angular/router';
-import data from '../data.json';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Data } from "../model/data";
+import dataFile from "../data/data.json";
+import { IonicSelectableComponent } from "ionic-selectable";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage implements OnInit {
   data: Data[];
   selected: Data;
+  valueField: number;
 
   constructor() {}
 
-  ngOnInit(){
-    this.data = data;
+  ngOnInit() {
+    this.data = dataFile;
     console.log(this.data);
   }
 
-  onItemSelected(event: CustomEvent){
-    this.selected = event.detail.value;
+  onItemSelected(event: CustomEvent) {
+    this.valueField = null;
+    console.log("promenio");
+    
   }
 
-  onValueInput(event: CustomEvent){
-    console.log(event.detail.value);
+  returnDesc() {
+    if (this.selected.lowerLimit && this.selected.upperLimit) {
+      if (
+        this.valueField >= this.selected.lowerLimit &&
+        this.valueField <= this.selected.upperLimit
+      ) {
+        return "Vrednosti su granicama normale.";
+      } else if (this.valueField < this.selected.lowerLimit) {
+        return "Vrednosti su snižene.";
+      } else if (this.valueField > this.selected.upperLimit) {
+        return "Vrednosti su povišene.";
+      }
+    } else if (this.selected.lowerLimit && !this.selected.upperLimit) {
+      if (this.valueField >= this.selected.lowerLimit) {
+        return "Vrednosti su granicama normale.";
+      } else {
+        return "Vrednosti su snižene.";
+      }
+    } else if (!this.selected.lowerLimit && this.selected.upperLimit) {
+      if (this.valueField <= this.selected.upperLimit) {
+        return "Vrednosti su granicama normale.";
+      } else {
+        return "Vrednosti su povišene.";
+      }
+    }
   }
 
+  returnDetails() {
+    if (this.selected.lowerLimit && this.selected.upperLimit) {
+      if (
+        this.valueField >= this.selected.lowerLimit &&
+        this.valueField <= this.selected.upperLimit
+      ) {
+        return this.selected.descIfNormal;
+      } else if (this.valueField < this.selected.lowerLimit) {
+        return this.selected.descIfLower;
+      } else if (this.valueField > this.selected.upperLimit) {
+        return this.selected.descIfHigher;
+      }
+    } else if (this.selected.lowerLimit && !this.selected.upperLimit) {
+      if (this.valueField >= this.selected.lowerLimit) {
+        return this.selected.descIfNormal;
+      } else {
+        return this.selected.descIfLower;
+      }
+    } else if (!this.selected.lowerLimit && this.selected.upperLimit) {
+      if (this.valueField <= this.selected.upperLimit) {
+        return this.selected.descIfNormal;
+      } else {
+        return this.selected.descIfHigher;
+      }
+    }
+  }
 }
--- src/app/model/data.ts
+++ src/app/model/data.ts
@@ -1,8 +1,13 @@
 export class Data {
-    constructor(
-        public id: number,
-        public name: string,
-        public lowerLimit: number,
-        public upperLimit: number
-    ){}
+  constructor(
+    public id: number,
+    public name: string,
+    public category: string,
+    public measurementUnit: string,
+    public lowerLimit?: number,
+    public upperLimit?: number,
+    public descIfNormal?: string,
+    public descIfLower?: string,
+    public descIfHigher?: string
+  ) {}
 }
