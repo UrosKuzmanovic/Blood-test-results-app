ee21f570b2116239ed7cef23d2a24e0e51137396
UrosKuzmanovic <ukuzmanovic1@gmail.com>
2020-12-01 12:49:26 +0100

    Reading from data.json, selecting data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
+import data from '../data.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  data: Data[];
+  selected: Data;
 
   constructor() {}
+
+  ngOnInit(){
+    this.data = data;
+    console.log(this.data);
+  }
+
+  onItemSelected(event: CustomEvent){
+    this.selected = event.detail.value;
+  }
+
+  onValueInput(event: CustomEvent){
+    console.log(event.detail.value);
+  }
 
 }
